93f1266393e8c43d7c848a630ad3ac3491467aa2
Ihor Manzii <igorcollpsp@gmail.com>
2023-12-19 19:33:28 +0100

    Init project

---
+++ src/main/java/com/ihormanzii/currency/checker/CurrencyCheckerApplication.java
@@ -0,0 +1,13 @@
+package com.ihormanzii.currency.checker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CurrencyCheckerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CurrencyCheckerApplication.class, args);
+	}
+
+}
